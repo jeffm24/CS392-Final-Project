36398e8fe8fa13c7df53a504cfc697808ed9d16a
seleesey <slee2@stevens.edu>
2014-12-05 06:04:52 -0500

    cut_eol

--- src/mynotsominishell/cut_eol.c
+++ src/mynotsominishell/cut_eol.c
@@ -6,16 +6,21 @@
  */
 
 void cut_eol() {
+  
   char *buff;
   char *start;
   buff = (char*)gl_env.currNode->elem;
 
   gl_env.clipboard = get_substring(buff, gl_env.pos, gl_env.size-1);
 
-  gl_env.currNode->elem = get_substring(buff, 0, gl_env.pos - 1);
-  gl_env.size = my_strlen(gl_env.currNode->elem);
+  gl_env.size = my_strlen(get_substring(buff, 0, gl_env.pos - 1));
+
+  remove_substring((char**)&gl_env.currNode->elem, gl_env.pos, gl_env.size-1);
   refresh_buff();
 
   //my_str("CUT: ");
   //my_str(gl_env.clipboard);
+  my_str("SIZE OF BUFF: ");
+  my_int(gl_env.size);
+
 }
