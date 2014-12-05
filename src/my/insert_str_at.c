a4802df8ce3827aeb86db24ac0f13b79a47e741e
jeffm24 <jmariconda24@gmail.com>
2014-12-05 07:51:19 -0500

    added some comments

--- src/my/insert_str_at.c
+++ src/my/insert_str_at.c
@@ -9,7 +9,6 @@
  */
 void insert_str_at(char **str, char *ins, unsigned int i)
 {
-  //char *tmp;
   char tmp[256];
   unsigned int nStr, nIns;
   unsigned int x, y, n;
@@ -19,47 +18,22 @@ void insert_str_at(char **str, char *ins, unsigned int i)
     nIns = my_strlen(ins);
 
     if (i < nStr) {
-      //tmp = (char*)xmalloc(nStr + nIns + 1);
-      
-      //tmp = my_strncpy(tmp, (*str), i);
-      //tmp[i] = '\0';
-      
       for (x = 0 ; x < i ; x++)
 	tmp[x] = (*str)[x];
 
-      //tmp = my_strcat(tmp, ins);
-      //tmp[i + nIns] = '\0';
-      
       for (y = 0 ; ins[y] != '\0' ; x++, y++)
 	tmp[x] = ins[y];
- 
-      //tmp = my_strcat(tmp, get_substring((*str), i, nStr));
-      
+       
       for (; (*str)[i] != '\0' ; x++, i++)
 	tmp[x] = (*str)[i];
       tmp[x] = '\0';
-
-      //(*str) = my_strcpy((*str), tmp);
-      
+     
       for (x = 0 ; tmp[x] != '\0' ; x++)
 	(*str)[x] = tmp[x];
       (*str)[x] = '\0';
 
-      //free(tmp);
     } else {
       (*str) = my_strcat((*str), ins);
     }
-    /*
-    for (x = 0, y = 0 ; x < n && y < n + 1 ; x++, y++) {
-      if (y == i)
-	(*s)[y++] = c;
-      (*s)[y] = tmp[x];
-    }
-    
-    if (y < n + 1)
-      (*s)[y++] = c;
-    
-    (*s)[y] = '\0';
-    */
   }
 }
--- src/my/remove_substring.c
+++ src/my/remove_substring.c
@@ -3,12 +3,11 @@
  * Author: Jeff Mariconda
  * Class: CS-392
  *
- * Pre: Takes a pointer to the address of the string, a start index, and an end index for the substring.
+ * Pre: Takes a pointer to the address of the string, a start index for the substring, and an end index for the substring.
  * Post: Removes the substring from the string.
  */
 void remove_substring(char **str, unsigned int s, unsigned int f)
 {
-  //char *tmp;
   char tmp[256];
   unsigned int x, n;
 
@@ -16,37 +15,16 @@ void remove_substring(char **str, unsigned int s, unsigned int f)
     n = my_strlen(*str);
     
     if (s < n) {
-      //tmp = (char*)xmalloc(n);
-      
-      //tmp = my_strncpy(tmp, (*str), s);
-
       for (x = 0 ; x < s ; x++) 
 	tmp[x] = (*str)[x];
-      
-      //tmp[s] = '\0';
-      //tmp = my_strcat(tmp, get_substring((*str), f + 1, n));
-      
+
       for (f += 1 ; f < n ; x++, f++)
 	tmp[x] = (*str)[f];
       tmp[x] = '\0';
-      
-      //(*str) = my_strcpy((*str), tmp);
-      
+       
       for (x = 0 ; tmp[x] != '\0' ; x++) 
 	(*str)[x] = tmp[x];
       (*str)[x] = '\0';
-
-      //free(tmp);
     }
-
-
-    /*
-    for (x = 0, y = 0 ; x < n && y < n - 1 ; x++, y++) {
-      if (x == i)
-	x++;
-      (*s)[y] = tmp[x];
-    }
-    (*s)[y] = '\0';
-    */
   }
 }
