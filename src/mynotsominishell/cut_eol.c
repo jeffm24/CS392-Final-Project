48f8a7c26023cde752588f3c5fcf9f05620fe3a5
seleesey <slee2@stevens.edu>
2014-12-05 07:51:47 -0500

    cut_eol finally fixed

--- src/mynotsominishell/cut_eol.c
+++ src/mynotsominishell/cut_eol.c
@@ -13,17 +13,19 @@ void cut_eol() {
   
   buff = (char*)gl_env.currNode->elem;
   trunc_size = my_strlen(get_substring(buff, 0, gl_env.pos - 1));
-  
   gl_env.clipboard = get_substring(buff, gl_env.pos, gl_env.size);
-  remove_substring((char**)&gl_env.currNode->elem, gl_env.pos, gl_env.size);
-  while (gl_env.pos < gl_env.size) {
-    curight();
-    
-  }
-  refresh_buff();
+  gl_env.clipboard[trunc_size] = '\0';
 
+  remove_substring((char**)&gl_env.currNode->elem, gl_env.pos, gl_env.size);
 
   gl_env.size = trunc_size;
+  while (gl_env.pos < gl_env.size)
+    curight();
+  
+  refresh_buff();
+
+  
+  
 
   //my_str("CUT: ");
   //my_str(gl_env.clipboard);
